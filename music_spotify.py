185060dcf46bed07a71e55a0b2697b53e19b2baa
Horacio Casimiro <horaciocasimiro@horacios-laptop.myfiosgateway.com>
2024-04-27 23:25:26 -0400

    music_spofity files were not pushed

--- music_spotify.py
+++ music_spotify.py
@@ -1,23 +1,31 @@
-from bot_utilities import connect_to_api_endpoint
+from bot_utilities import connect_to_api_endpoint,check_if_elapsed_time_passed
+from datetime import datetime
 
 class Spotify():
+    client_id = '90d4133dd76d4b2c8adec68c9e302924'
+    client_secret = None
+
     access_token = None
+    access_token_datetime = None
 
     
     def __init__(self, client_secret):
-        client_id = '90d4133dd76d4b2c8adec68c9e302924'
+        self.client_secret = client_secret
+        self.get_access_token()
+
+    def get_access_token(self):
         header = {
             'Content-Type' : 'application/x-www-form-urlencoded',
         }
         request_body = {
             'grant_type' : f'client_credentials',
-            'client_id' : client_id,
-            'client_secret' : client_secret
+            'client_id' : self.client_id,
+            'client_secret' : self.client_secret
         }
-        result = connect_to_api_endpoint(http_method='POST', url='https://accounts.spotify.com/api/token',header_dict=header,data=request_body)
+        result = connect_to_api_endpoint(http_method='POST', url='https://accounts.spotify.com/api/token',header=header,data=request_body)
         self.access_token = f"Bearer {result.get('access_token')}"
-
-        print(f'works {self.access_token}')
+        self.access_token_datetime = datetime.now()
+    
     def get_track_names_and_artist(self, tracks_list):
         result_list = []
         print(f'In the music_spotify: {tracks_list}')
@@ -40,8 +48,11 @@ class Spotify():
             'offset' : '5',
             'type' : 'track'
         }
+        access_token_expired = check_if_elapsed_time_passed(self.access_token_datetime,datetime.now(), 60)
+        if access_token_expired:
+            self.get_access_token()
         header_params = {
             'Authorization' : self.access_token
         }
-        search_result = connect_to_api_endpoint(http_method='GET', url=url, query_parameters_dict=query_params,header_dict=header_params)
+        search_result = connect_to_api_endpoint(http_method='GET', url=url, query_parameters=query_params,header=header_params)
         return search_result['tracks']['items']
