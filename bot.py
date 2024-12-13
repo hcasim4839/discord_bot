8cd72cfcb06ddd001093e2fbd0e1529aaa318a10
hcasim4839 <casimiroh3@gmail.com>
2024-12-12 19:31:49 -0500

    made cache more explicit and re-organzied code

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from bot_utilities import get_secret, check_if_elapsed_time_passed
 from datetime import datetime
 
 access_token_cache = {
+    'access_token_discord': None
 }
 client_secrets_cache = {
     'client_secret_spotify': None
@@ -14,7 +15,7 @@ intents.message_content = True
 client = Client(intents=intents)
 
 #setting up credentials and caching them
-if 'access_token_discord' in access_token_cache:
+if access_token_cache.get('access_token_discord'):
     has_elapsed_time_passed = check_if_elapsed_time_passed(access_token_cache.get('access_token_discord'), datetime.now(), 360)
     if has_elapsed_time_passed:
         token = get_secret('access_token_discord','access-token')
